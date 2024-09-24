3989eeaa89290d37f7d16f5de0c09ed6440d8883
knull <kelanidarasimi9@gmail.com>
2024-09-24 09:11:14 +0100

    dart basic

---
+++ const_final.dart
@@ -0,0 +1,11 @@
+void main() {
+  // const can be useful when a varaible won't change accidentally
+  // while final can be set only once during runtime;
+
+  final age = 25;
+  const name = "Mahdi";
+
+  dynamic man = "Bayo";
+  man = 12;
+  print(man);
+}
---
+++ main.dart
@@ -0,0 +1,32 @@
+// ~/ is used for integer dividing
+
+// % modulo operator
+void main() {
+  // print((4 + 4) ~/ 3);
+
+  // print(4 % 2);
+
+  // variables in dart
+  int myValue = 1;
+  print(myValue);
+
+  // string
+  String name = "Darasimi";
+  print(name);
+
+  // double
+  double price = 3.0;
+  print(price);
+
+  // boolean
+  bool myBool = true;
+  print(myBool);
+
+  // num is a variable used for holding both double and integer datatype
+  num myInteger = 1;
+  print(myInteger);
+
+  var noType = 12;
+  print(noType);
+
+}
