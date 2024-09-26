6ee1bbb0c491cc256a9ea436c5062316fc243578
knull <kelanidarasimi9@gmail.com>
2024-09-26 09:14:16 +0100

    exercise 6

---
+++ practice_dart/ex6.dart
@@ -0,0 +1,27 @@
+import 'dart:io';
+
+void main() {
+  print("Enter your word to get if it's a palindrome");
+  final word = stdin.readLineSync();
+
+  String ans =
+      isPalindrome(word!) ? "It's a palindrome" : "It's not a palindrome";
+
+  print(ans);
+}
+
+bool isPalindrome(String str) {
+  if (str.isEmpty) return false;
+
+  int len = str.length - 1;
+  for (int i = 0; i < str.length; i++) {
+    if (len >= 0 && str[i] == str[len]) {
+      len--;
+      continue;
+    } else {
+      return false;
+    }
+  }
+
+  return true;
+}
