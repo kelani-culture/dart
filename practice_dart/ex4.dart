a3efc78d9ac5e14af2bebc71b5cac7190e7afc10
knull <kelanidarasimi9@gmail.com>
2024-09-26 08:52:55 +0100

    practicing dart

--- practice_dart/ex4.dart
+++ practice_dart/ex4.dart
@@ -1,1 +1,16 @@
+import 'dart:io';
 
+void main() {
+  List<int> divisors = [];
+
+  print("Enter your digits");
+  int num = int.parse(stdin.readLineSync()!);
+
+  int dividened = 1;
+
+  while (dividened < num) {
+    if (num % dividened == 0) divisors.add(dividened);
+    dividened++;
+  }
+  print("Your divisors are $divisors");
+}
---
+++ practice_dart/ex5.dart
@@ -0,0 +1,7 @@
+void main() {
+  List<int> a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+  List<int> b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+
+  Set<int> difference = a.toSet().intersection(b.toSet());
+  print(difference);
+}
